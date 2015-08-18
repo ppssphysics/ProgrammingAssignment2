eb8646e8888135aec4937636248fde66b7c0be9d
ppssphysics <psaouter@icloud.com>
2015-08-18 09:22:24 +0200

    First Results Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,72 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 18 August 2015 - Data Science Specialization
+## RProgramming / Week2 / ProgrammingAssignment2 
 
-# My first comment to test new branch push to GitHub
-# Now the other way back fetching to local
+## Test Example
+## > tstmtx <- matrix(c(0, 7, -1, 0), 2, 2)
+## > a <- makeCacheMatrix(tstmtx)
+## > cacheSolve(a)
+## > cacheSolve(a) --> will return result from cahed data
 
-## Write a short comment describing this function
+
+
+
+## makeCacheMatrix()
+## Function creates special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    ## Check that matrix is invertible, else quit
+    if(det(x)==0) {
+        message("matrix passed is not invertible (det(x) == 0). Returned Dummy!")
+        return(NULL)
+    } 
+    
+    # returned object initialization
+    m <- NULL
+    
+    ## set() : set value of matrix in cashe
+    ## get() : get value of matrix from cashe
+    
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    
+    ## setinv() : set inverse of matrix in cashe
+    ## getinv() : get inverse of matrix from cashe
+    
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    
+    ## Create list of functions to access set,get,setinv,getinv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve()
+## Function computes inverse of special "matrix" returned by makeCacheMatrix. 
+## If inverse already calculated, cachesolve retrieves inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    ## Associate getinv() of x (might not be defined)
+    m <- x$getinv()
+    
+    ## if getinv() for x defined in cashe (!=NULL), return inverse from cashe
+    if(!is.null(m)) { 
+        message("getting cached data")
+        return(m)
+    }
+    
+    ## if getinv() for x does not yet defined in cashe :
+    ## 1. new matrix set in the cashe
+    ## 2. get inverse of x using solve
+    ## 3. set inverse of x in cashe
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    
+    # return inverse matrix
+    m
 }
---
+++ cachevector.R
@@ -0,0 +1,47 @@
+
+## 18 August 2015 - Data Science Specialization
+## RProgramming / Week2 / ProgrammingAssignment2 
+## Test Example fro assigment 
+
+## a <- makeVector(18:45)
+## cachemean(a)
+
+## makeVector creates special "vector" : list containing function to
+## 1. set the value of the vector
+## 2. get the value of the vector
+## 3. set the value of the mean
+## 4. get the value of the mean
+
+makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+}
+
+
+## The following function calculates the mean of the special "vector" created 
+## with the above function. However, it first checks to see if the mean has 
+## already been calculated. If so, it gets the mean from the cache and skips 
+## the computation. Otherwise, it calculates the mean of the data and sets the 
+## value of the mean in the cache via the setmean function.
+
+cachemean <- function(x, ...) {
+    
+    m <- x$getmean()
+    if(!is.null(m)) { # if getmean() for x exists in cashe
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+}
